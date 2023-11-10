baea590e237c4fd3914f3ae0be2c6c33e7d95d1c
vladimir.petukhov@inrix.com <vladimir.petukhov@mynehub.com>
2023-11-10 10:32:39 +0200

    add: create project structure

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
     { path: '', redirectTo: 'home', pathMatch: 'full' },
-    { path: 'home', loadChildren: () => import('app/home/home.module').then(m => m.HomeModule) }
+    { path: 'home', loadChildren: () => import('app/home/home.module').then(m => m.HomeModule) },
+    { path: 'auth', loadChildren: () => import('app/auth/auth.module').then(m => m.AuthModule) },
+    { path: 'billing', loadChildren: () => import('app/billing/billing.module').then(m => m.BillingModule) },
+    { path: 'invertory', loadChildren: () => import('app/inventory/inventory.module').then(m => m.InventoryModule) },
+    { path: 'profile', loadChildren: () => import('app/profile/profile.module').then(m => m.ProfileModule) },
+    { path: 'reports', loadChildren: () => import('app/reports/reports.module').then(m => m.ReportsModule) }
 ];
 
 @NgModule({
